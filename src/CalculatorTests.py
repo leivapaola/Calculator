96586e2ba36a7caa0086aa23c6884d3bed9f4aaf
leivapaola <78447640+leivapaola@users.noreply.github.com>
2021-03-22 12:14:33 -0400

    Fix | CalculatorTest Feature | added addition.py page

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,60 +1,75 @@
 import unittest
+import csv
+from Calculator import Calculator
 
-from Calculator.Calculator import Calculator
-from CSVReader.CSVReader import CsvReader
+test_data_file_object = None
+
+
+def load_test_data_into_list(test_data_file_path):
+    global test_data_file_object
+    my_list = list()
+    test_data_file_object = open(test_data_file_path, 'r')
+    csv_reader = csv.reader(test_data_file_object, delimiter=',')
+    for row in csv_reader:
+        my_list.append(row)
+    test_data_file_object.close()
+    test_data_file_object = None
+    return my_list
 
 
 class MyTestCase(unittest.TestCase):
+
     def setUp(self) -> None:
         self.calculator = Calculator()
+
+    def tearDown(self) -> None:
+        it self.calculator is not None:
+            self.calculator = None
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_addition(self):
-        test_data = CsvReader("src/addition.csv").data
+    def test_add_method_calculator(self):
+        test_data = load_test_data_into_list('../src/addition.csv').data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_subtraction(self):
-        test_data = CSVReader("src/Subtraction.csv").data
+        test_data = load_test_data_into_list('../src/Subtraction.csv").data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_multiplication(self):
-        test_data = CsvReader("src/Multiplication.csv").data
+        test_data = load_test_data_into_list('../src/Multiplication.csv').data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_division(self):
-        test_data = CsvReader("src/Division.csv").data
+        test_data = load_test_data_into_list('../src/Division.csv').data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_Square(self):
-        test_data = CsvReader("src/Square.csv").data
+        test_data = load_test_data_into_list('../src/Square.csv').data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.square(row['Value 1'], int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_SquareRoot(self):
-        test_data = CsvReader("src/Squareroot.csv").data
+        test_data = load_test_data_into_list('../src/Squareroot.csv').data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.square(row['Value 1'], int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
-
-    def test_results_property_calculator(self):
-        self.assertEqual(self.calculator.result, 0)
 
 
 if __name__ == '__main__':
